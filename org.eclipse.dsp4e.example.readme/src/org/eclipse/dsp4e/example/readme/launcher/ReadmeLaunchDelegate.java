d5876b8d2bc07f6a53c0730268e62aa95e054da1
Tracy Miranda <tracy@kichwacoders.com>
2017-07-28 16:03:22 +0100

    Initial debug plugin

---
+++ org.eclipse.dsp4e.example.readme/src/org/eclipse/dsp4e/example/readme/launcher/ReadmeLaunchDelegate.java
@@ -0,0 +1,21 @@
+package org.eclipse.dsp4e.example.readme.launcher;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+public class ReadmeLaunchDelegate implements ILaunchConfigurationDelegate {
+
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
+		
+		System.out.println("Ready to debug something");
+		//DebugPlugin.newProcess(launch, process, label)
+
+		//create a debug target
+	}
+
+}
---
+++ org.eclipse.dsp4e.example.readme/src/org/eclipse/dsp4e/example/readme/launcher/ReadmeTabGroup.java
@@ -0,0 +1,16 @@
+package org.eclipse.dsp4e.example.readme.launcher;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
+
+public class ReadmeTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		setTabs(new ILaunchConfigurationTab[] { new CommonTab() });
+	}
+
+}
